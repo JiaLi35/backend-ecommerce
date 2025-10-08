780885c202dc8c6c235e7d7859ce595553401ecd
Jia Li <chin.j3li@gmail.com>
2025-10-08 14:45:48 +0800

    add mongodb url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 async function connectToMongoDB() {
   try {
     // wait for the MongoDB to connect
-    await mongoose.connect("mongodb://localhost:27017/ecommerce");
+    await mongoose.connect(process.env.MONGODB_URL + "/ecommerce");
     console.log("MongoDB is connected");
   } catch (error) {
     console.log(error);
